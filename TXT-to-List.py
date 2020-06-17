24697f7a2f290c77a003a4022d482f3bcba978d1
Janik <janik@klauenberg.eu>
2020-06-17 20:11:01 +0200

    added gitignore

--- TXT-to-List.py
+++ TXT-to-List.py
@@ -2,13 +2,16 @@
 __github__ =
 from io import open
 #Hier drunter befinden sich alle Variablen
-filename = "test.txt"
 
-f = open(filename, "r", encoding='utf-8')
-x = f.read()
-f.close()
-res = []
-for i in x:
-    res.append(i)
+def get_content(filename):
+    f = open(filename, "r", encoding='utf-8')
+    x = f.read()
+    f.close()
+    res = []
+    for i in x:
+        res.append(i)
+    return res
 
+if __name__ == '__main__':
+    get_content("test.txt")
 print (res) #Auskommentieren wenn kein output via Print gewünscht
